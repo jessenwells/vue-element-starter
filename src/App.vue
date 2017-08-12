794d20b207f36656e388e1f96c17238765dfde30
Jesse <jessenwells@gmail.com>
2017-08-12 15:59:12 -0400

    First!

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 {{ msg }}
+    el-button(@click.native="startHacking") Yes!
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Want something new?'
+    }
+  },
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'Shhh',
+        message: 'Just be patient...',
+        duration: 6000
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family sans-serif
+  text-align center
+</style>
+
